da8d982a49a56a3adfdd0214885f1d6492cf9214
Alexander-Muendesi <u20426918@tuks.co.za>
2023-09-05 15:01:35 +0200

    🐛 Fixed a major bug

--- Heuristic_Two/src/GE/GrammarRules.java
+++ Heuristic_Two/src/GE/GrammarRules.java
@@ -20,178 +20,178 @@ public class GrammarRules {
         grammar.put("<start>",temp);
 
         //<accept>
-        temp.clear(); vals.clear();
+        temp = new HashMap<Integer,List<String>>(); vals = new ArrayList<String>();
         vals.add("IO"); 
-        temp.put(0,vals); vals.clear();
+        temp.put(0,vals); vals = new ArrayList<String>();
 
         vals.add("AM");
         temp.put(1,vals);
         grammar.put("<accept>",temp);
 
         //<heuristic>
-        temp.clear();vals.clear();
+        temp = new HashMap<Integer,List<String>>();vals = new ArrayList<String>();
         vals.add("swap"); vals.add("<n>"); vals.add("<compSel>");
-        temp.put(0, vals); vals.clear();
+        temp.put(0, vals); vals = new ArrayList<String>();
 
         vals.add("move"); vals.add("<n>"); vals.add("<compSel>");
-        temp.put(1, vals); vals.clear();
+        temp.put(1, vals); vals = new ArrayList<String>();
 
         vals.add("add"); vals.add("<n>"); vals.add("<compSel>");
-        temp.put(2,vals); vals.clear();
+        temp.put(2,vals); vals = new ArrayList<String>();
 
         vals.add("delete"); vals.add("<n>"); vals.add("<compSel>");
-        temp.put(3,vals); vals.clear();
+        temp.put(3,vals); vals = new ArrayList<String>();
 
         vals.add("shuffle"); vals.add("<n>"); vals.add("<compSel>");
-        temp.put(4, vals); vals.clear();
+        temp.put(4, vals); vals = new ArrayList<String>();
 
         vals.add("<heuristic>"); vals.add("<cop>"); vals.add("<heuristic>");
-        temp.put(5, vals); vals.clear();
+        temp.put(5, vals); vals = new ArrayList<String>();
 
         vals.add("<heuristic>");
-        temp.put(6, vals); vals.clear();
+        temp.put(6, vals); vals = new ArrayList<String>();
 
         vals.add("if"); vals.add("<cond>"); vals.add("<heuristic>"); vals.add("<heuristic>");
-        temp.put(7,vals); vals.clear();
+        temp.put(7,vals); vals = new ArrayList<String>();
 
-        grammar.put("<heuristic>", temp); temp.clear();
+        grammar.put("<heuristic>", temp); temp = new HashMap<Integer,List<String>>();
 
         //<cond>
         vals.add("<rop>"); vals.add("<h_value>"); vals.add("<h_value>");
-        temp.put(0, vals); vals.clear();
+        temp.put(0, vals); vals = new ArrayList<String>();
 
         vals.add("if"); vals.add("<cond>"); vals.add("<cond>"); vals.add("<cond>");
-        temp.put(1, vals); vals.clear();
+        temp.put(1, vals); vals = new ArrayList<String>();
 
-        grammar.put("<cond>", temp); temp.clear();
+        grammar.put("<cond>", temp); temp = new HashMap<Integer,List<String>>();
 
         //h_value
         vals.add("prevFitness");
-        temp.put(0,vals); vals.clear();
+        temp.put(0,vals); vals = new ArrayList<String>();
 
         vals.add("currFitness");
-        temp.put(1, vals); vals.clear();
+        temp.put(1, vals); vals = new ArrayList<String>();
 
         vals.add("diffFitness");
-        temp.put(2, vals); vals.clear();
+        temp.put(2, vals); vals = new ArrayList<String>();
 
         vals.add("currIteration");
-        temp.put(3, vals); vals.clear();
+        temp.put(3, vals); vals = new ArrayList<String>();
 
         vals.add("totalIterations");
-        temp.put(4,vals); vals.clear();
+        temp.put(4,vals); vals = new ArrayList<String>();
 
-        grammar.put("<h_value>", temp); temp.clear();
+        grammar.put("<h_value>", temp); temp = new HashMap<Integer,List<String>>();
 
         //<compSel>
         vals.add("lowestCost");
-        temp.put(0,vals); vals.clear();
+        temp.put(0,vals); vals = new ArrayList<String>();
 
         vals.add("highestCost");
-        temp.put(1, vals); vals.clear();
+        temp.put(1, vals); vals = new ArrayList<String>();
 
         vals.add("smallestSize");
-        temp.put(2, vals); vals.clear();
+        temp.put(2, vals); vals = new ArrayList<String>();
 
         vals.add("largestSize");
-        temp.put(3, vals); vals.clear();
+        temp.put(3, vals); vals = new ArrayList<String>();
 
         vals.add("random");
-        temp.put(4, vals); vals.clear();
+        temp.put(4, vals); vals = new ArrayList<String>();
 
         vals.add("if"); vals.add("<prob>"); vals.add("<compSel>"); vals.add("<compSel>");
-        temp.put(5, vals); vals.clear();
+        temp.put(5, vals); vals = new ArrayList<String>();
 
-        grammar.put("<compSel>", temp); temp.clear();
+        grammar.put("<compSel>", temp); temp = new HashMap<Integer,List<String>>();
 
         //<comp>
         vals.add("lecture");
-        temp.put(0, vals); vals.clear();
+        temp.put(0, vals); vals = new ArrayList<String>();
 
         vals.add("period");
-        temp.put(1, vals); vals.clear();
+        temp.put(1, vals); vals = new ArrayList<String>();
 
         vals.add("room");
-        temp.put(2, vals); vals.clear();
+        temp.put(2, vals); vals = new ArrayList<String>();
 
-        grammar.put("<comp>", temp); temp.clear();
+        grammar.put("<comp>", temp); temp = new HashMap<Integer,List<String>>();
 
         //cop
         vals.add("union");
-        temp.put(0,vals); vals.clear();
+        temp.put(0,vals); vals = new ArrayList<String>();
 
         vals.add("rGradient");
-        temp.put(1, vals); vals.clear();
+        temp.put(1, vals); vals = new ArrayList<String>();
 
-        grammar.put("<cop>", temp); temp.clear();
+        grammar.put("<cop>", temp); temp = new HashMap<Integer,List<String>>();
 
         //rop
         vals.add("<=");
-        temp.put(0,vals); vals.clear();
+        temp.put(0,vals); vals = new ArrayList<String>();
 
         vals.add("<");
-        temp.put(1, vals); vals.clear();
+        temp.put(1, vals); vals = new ArrayList<String>();
 
         vals.add(">");
-        temp.put(2,vals); vals.clear();
+        temp.put(2,vals); vals = new ArrayList<String>();
 
         vals.add(">=");
         temp.put(3,vals);
 
-        grammar.put("<rop>",temp); temp.clear();
+        grammar.put("<rop>",temp); temp = new HashMap<Integer,List<String>>();
 
         //prob
         vals.add("25");
-        temp.put(0,vals); vals.clear();
+        temp.put(0,vals); vals = new ArrayList<String>();
 
         vals.add("50");
-        temp.put(1, vals); vals.clear();
+        temp.put(1, vals); vals = new ArrayList<String>();
 
         vals.add("75");
         temp.put(2,vals);
 
-        grammar.put("<prob>", temp); temp.clear();
+        grammar.put("<prob>", temp); temp = new HashMap<Integer,List<String>>();
 
         //<n>
 
         vals.add("1");
-        temp.put(0,vals); vals.clear();
+        temp.put(0,vals); vals = new ArrayList<String>();
 
         vals.add("2");
-        temp.put(1, vals); vals.clear();
+        temp.put(1, vals); vals = new ArrayList<String>();
 
         vals.add("3");
-        temp.put(2, vals); vals.clear();
+        temp.put(2, vals); vals = new ArrayList<String>();
 
         vals.add("4");
-        temp.put(3, vals); vals.clear();
+        temp.put(3, vals); vals = new ArrayList<String>();
 
         vals.add("5");
-        temp.put(4, vals); vals.clear();
+        temp.put(4, vals); vals = new ArrayList<String>();
 
         vals.add("6");
-        temp.put(5, vals); vals.clear();
+        temp.put(5, vals); vals = new ArrayList<String>();
 
         vals.add("7");
-        temp.put(6,vals); vals.clear();
+        temp.put(6,vals); vals = new ArrayList<String>();
 
         vals.add("8");
-        temp.put(7, vals); vals.clear();
+        temp.put(7, vals); vals = new ArrayList<String>();
 
         vals.add("9");
-        temp.put(8, vals); vals.clear();
+        temp.put(8, vals); vals = new ArrayList<String>();
 
         vals.add("10");
-        temp.put(9,vals); vals.clear();
+        temp.put(9,vals); vals = new ArrayList<String>();
 
         vals.add("<n>");
-        temp.put(10, vals); vals.clear();
+        temp.put(10, vals); vals = new ArrayList<String>();
 
         vals.add("<n>");
         vals.add("<n>");
-        temp.put(11, vals); vals.clear();
+        temp.put(11, vals); vals = new ArrayList<String>();
 
-        grammar.put("<n>", temp); temp.clear();
+        grammar.put("<n>", temp); temp = new HashMap<Integer,List<String>>();
     }
 
     /**
@@ -210,5 +210,9 @@ public class GrammarRules {
      */
     public boolean containsKey(String val){
         return this.grammar.containsKey(val);
+    }
+
+    public int getSize(){
+        return this.grammar.size();
     }
 }
